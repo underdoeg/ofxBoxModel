9d5cb064021c99f5403acf08207886fb42c98ba0
Philip Whitfield <public@underdoeg.com>
2013-05-26 19:33:54 +0200

    get value as types

--- src/core/Serializable.h
+++ src/core/Serializable.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <map>
+#include "BoxModel.h"
 
 namespace boxModel
 {
@@ -18,15 +19,34 @@ public:
 		Value(std::string v){
 			value = v;
 		}
+		
+		int asInt(){
+			return stringToInt(value.c_str());
+		}
+		
+		float asFloat(){
+			return stringToFloat(value.c_str());
+		}
+		
+		Point asPoint(){
+			Point ret;
+			std::vector<std::string> parts = stringSplit(value, " ");
+			if(parts.size()>0)
+				ret.x = stringToFloat(parts[0]);
+			if(parts.size()>1)
+				ret.y = stringToFloat(parts[1]);
+			return ret;
+		}
+		
 		std::string value;
 	};
 	
-	typedef std::map<std::string, Value> KeyValueList;
+	typedef std::map<std::string, Value> ValuleMap;
 
 	Serializable(){};
 	~Serializable(){};
 	
-	KeyValueList getSerialized(){
+	ValuleMap getSerialized(){
 		
 	}
 };
